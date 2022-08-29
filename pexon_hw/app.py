f1e0a97d46165b80af9d65b78f6079a9cd23ac84
bikls <xdenisbauerx@gmail.com>
2022-08-29 14:22:28 +0200

    Dockerfile und docker-compose added

--- pexon_hw/app.py
+++ pexon_hw/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # DB Erstellung
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zertifikate.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/zertifikate.db'
 db = SQLAlchemy(app)
 
 class Zertifikate(db.Model):
@@ -38,4 +38,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
